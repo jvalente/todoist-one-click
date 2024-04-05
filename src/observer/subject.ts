823ba48c71c27b83f612974972b998adcdd10a97
João Valente <joao@doist.com>
2024-04-06 01:49:30 +0200

    Add observer subject utility

---
+++ src/observer/subject.ts
@@ -0,0 +1,20 @@
+type Observer<T> = (data: T) => void
+
+export default class Subject<T> {
+    observers: Observer<T>[] = []
+
+    attach(observer: Observer<T>) {
+        this.observers.push(observer)
+    }
+
+    detach(observer: Observer<T>) {
+        const index = this.observers.indexOf(observer)
+        if (index > -1) {
+            this.observers.splice(index, 1)
+        }
+    }
+
+    notify(data: T) {
+        this.observers.forEach((observer) => observer(data))
+    }
+}
