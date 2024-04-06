87d508f01876ad3e96561fba47f00372ea3f8696
João Valente <joao@doist.com>
2024-04-06 15:46:16 +0200

    Update projects and targetLabel models

--- src/models/target-labels.ts
+++ src/models/target-labels.ts
@@ -1,30 +1,11 @@
-import Storage from '../storage/storage.js'
+import Model from './model.js'
 
-function get() {
-    return Storage.get<string[]>('targetLabels').then(
-        (targetLabels) => targetLabels
-    )
+class TargetLabelsModel extends Model<any> {
+    constructor() {
+        super('targetLabels')
+    }
 }
 
-function set(targetLabels: string[]) {
-    return Storage.set('targetLabels', targetLabels)
-}
-
-function addChangeListener(callback: (targetLabels: string[]) => void) {
-    chrome.storage.local.onChanged.addListener((changes) => {
-        if (
-            changes.targetLabels &&
-            changes.targetLabels.newValue !== changes.targetLabels.oldValue
-        ) {
-            callback(changes.targetLabels.newValue)
-        }
-    })
-}
-
-const TargetLabels = {
-    get,
-    set,
-    addChangeListener,
-}
+const TargetLabels = new TargetLabelsModel()
 
 export default TargetLabels
