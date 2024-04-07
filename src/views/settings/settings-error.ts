ae191dda23670dbcd27c1585b4d3c6022b5870cb
João Valente <joao@doist.com>
2024-04-07 16:39:32 +0200

    Add more information to the error section

--- src/views/settings/settings-error.ts
+++ src/views/settings/settings-error.ts
@@ -11,11 +11,38 @@ export class SettingsErrorElement extends LitElement {
     @property()
     error: any = undefined
 
+    private getErrorDescription(error: any) {
+        if (error && error.status === 401) {
+            return html`<small
+                >This error usually happens when the API token is invalid or
+                expired. Try to update it using the option below.</small
+            >`
+        }
+
+        if (
+            error &&
+            (error.message.toLowerCase() === 'failed to fetch' ||
+                error.message.toLowerCase() ===
+                    'networkerror when attempting to fetch resource.')
+        ) {
+            return html`<small
+                >Looks like there's an issue connecting to Todoist. Is your
+                internet working alright?</small
+            >`
+        }
+
+        return null
+    }
+
     render() {
         if (this.error) {
             return html`<section>
                 <label>Something went wrong...</label>
-                <p>${this.error?.message}</p>
+                ${this.getErrorDescription(this.error)}
+                <small style="font-family: monospace;font-style: italic;"
+                    >${this.error?.message}</small
+                >
+                <a href="#" @click=${() => window.location.reload()}>Retry</a>
             </section>`
         }
     }
