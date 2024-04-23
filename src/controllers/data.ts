fbff704a998671c7e48b47f36943ffd76d10d14e
João Valente <joao@doist.com>
2024-04-23 23:49:41 +0200

    Simplify data controller

--- src/controllers/data.ts
+++ src/controllers/data.ts
@@ -1,13 +1,8 @@
-import Projects from '../models/projects'
-import TargetProjectId from '../models/target-project-id'
 import { deleteAPIKey } from './api-key'
 import { Storage } from '../api/extension'
 
 export function clearAllData() {
     deleteAPIKey()
-
-    Projects.delete()
-    TargetProjectId.delete()
 
     Storage.reset()
 }
