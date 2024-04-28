022de37fdf526727252c1ea7874f36e7fde39b20
João Valente <joao@doist.com>
2024-04-28 23:15:14 +0200

    Draft advanced rules list item

--- src/views/settings/advanced-rules/advanced-rule.ts
+++ src/views/settings/advanced-rules/advanced-rule.ts
@@ -1,115 +1,38 @@
-import { LitElement, css, html } from 'lit'
+import { LitElement, html } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 
-import type { Rule } from '../../../types/rules.types'
-import type { InputChangeEvent, SelectChangeEvent } from '../../common/system'
-
 import type { ProjectsState } from '../../../types/projects.types'
+import type { Rule } from '../../../types/rules.types'
 
 import '../../common/system'
-import '../target-labels/target-labels-list'
-import '../target-project/target-project-select'
 
 @customElement('tc-advanced-rule')
 class AdvancedRuleElement extends LitElement {
-    static styles = [
-        css`
-            div > {
-                margin: 5;
-            }
-
-            div {
-                display: flex;
-                align-items: center;
-                gap: 8px;
-                margin: 5px 0;
-            }
-
-            tc-text-input {
-                flex-grow: 1;
-            }
-        `,
-    ]
+    @property({ type: Object })
+    rule!: Rule
 
     @property({ type: Array })
-    projects: ProjectsState['data'] = []
+    projects?: ProjectsState['data'] = []
 
-    @property({ type: Object })
-    rule: Omit<Rule, 'id'> = { matchMode: 'contains' }
-
-    get query() {
-        return this.rule.query || ''
-    }
-
-    private updateRule(update: Partial<Omit<Rule, 'id'>>) {
-        this.rule = { ...this.rule, ...update }
-    }
-
-    private updateQuery(event: InputChangeEvent) {
-        this.updateRule({ query: event.value })
-    }
-
-    private updateProject(event: CustomEvent) {
-        this.updateRule({
-            projectId: event.detail.projectId,
+    private handleEditRule() {
+        const customEvent = new CustomEvent('editRule', {
+            detail: { ruleId: this.rule.id },
+            bubbles: true,
+            composed: true,
         })
+
+        this.dispatchEvent(customEvent)
     }
 
-    private updateLabels(event: CustomEvent) {
-        this.updateRule({
-            labels: event.detail.labels,
-        })
-    }
-
-    private updateDueDate(event: InputChangeEvent) {
-        this.updateRule({
-            dueDate: event.value,
-        })
-    }
-
-    private saveRule() {
-        // biome-ignore lint/suspicious/noConsoleLog: <explanation>
-        console.log(this.rule)
+    private get projectName() {
+        const project = this.projects?.find((p) => p.id === this.rule.projectId)
+        return project?.name || 'Inbox'
     }
 
     render() {
-        return html`
-            <div>
-                <tc-text small>If the tab url</tc-text>
-                <tc-select
-                    small
-                    .options=${['contains', 'is exactly', 'matches regex']}
-                    .selectedValue=${'contains'}
-                    @change=${(matchMode: SelectChangeEvent<any>) =>
-                        this.updateRule({ matchMode: matchMode.selectedValue })}
-                ></tc-select>
-                <tc-text-input
-                    small
-                    .value=${this.query}
-                    placeholder="url"
-                    @change=${this.updateQuery}
-                ></tc-text-input>
-            </div>
-            <tc-text small>then add to project:</tc-text>
-            <tc-project-select
-                small
-                .rule=${this.rule}
-                .projects=${this.projects}
-                @change=${this.updateProject}
-            ></tc-project-select>
-            <tc-text small>then add labels:</tc-text>
-            <tc-target-labels-list
-                small
-                .labels=${this.rule.labels}
-                @change=${this.updateLabels}
-            ></tc-target-labels-list>
-            <tc-text small>and due date:</tc-text>
-            <tc-text-input
-                small
-                placeholder="due date..."
-                @change=${this.updateDueDate}
-            ></tc-text-input>
-            <button @click=${this.saveRule}>Save</button>
-        `
+        return html`<p>
+            ${this.rule.matchMode}: ${this.rule.query} - ${this.projectName}
+            <tc-link @click=${this.handleEditRule}>Edit</tc-link>
+        </p>`
     }
 }
