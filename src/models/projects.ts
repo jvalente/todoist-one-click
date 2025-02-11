9e02476ff1b5e53fae6d9d88a926976734d75381
João Valente <joao@doist.com>
2025-02-11 19:21:07 +0000

    Add ProjectModel getters

--- src/models/projects.ts
+++ src/models/projects.ts
@@ -5,6 +5,18 @@ class ProjectModel extends Model<Array<Project>> {
     constructor() {
         super('projects', { fetchResource: { url: 'projects' } })
     }
+
+    getAllNames() {
+        return this.get().then((projects) =>
+            projects?.map((project) => project.name)
+        )
+    }
+
+    getByName(projectName: string) {
+        return this.get().then((projects) =>
+            projects?.find((project) => project.name === projectName)
+        )
+    }
 }
 
 const Projects = new ProjectModel()
